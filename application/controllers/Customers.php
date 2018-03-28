06755e8b90a25b1621bc2ee076f6954bc8defbb7
bsizemore <bsizemore@apple.com>
2018-03-28 11:27:23 -0400

    customers table

--- application/controllers/Customers.php
+++ application/controllers/Customers.php
@@ -13,7 +13,51 @@ class Customers extends Stela {
   {
     $this->load->model('customers_model');
     $customers = $this->customers_model->get_customers();
+    echo"
+      <h1 class=customersHeader>Customers</h1>
+      <table class='table table-striped'>
+        <thead class='thead-dark'>
+          <tr>
+            <th scope='col'>#</th>
+            <th scope='col'>First</th>
+            <th scope='col'>Last</th>
+            <th scope='col'>Email</th>
+            <th scope='col'>Address 1</th>
+            <th scope='col'>Address 2</th>
+            <th scope='col'>City</th>
+            <th scope='col'>State</th>
+            <th scope='col'>Zip</th>
+            <th scope='col'>Phone</th>
+            <th scope='col'>Email Promotion</th>
+            <th scope='col'>Text Promotion</th>
+            <th scope='col'>Appointment Reminder</th>
+          </tr>
+        </thead>
+      <tbody>
+    ";
     foreach($customers as $c)
-      $this->dump_array($c);
+    {
+      echo"
+        <tr>
+          <th scope='row'><button type='button' class='btn btn-dark' id='customerEditButton_{$c['id']}'>Edit</button></th>
+          <td>{$c['firstName']}</td>
+          <td>{$c['lastName']}</td>
+          <td>{$c['email']}</td>
+          <td>{$c['address1']}</td>
+          <td>{$c['address2']}</td>
+          <td>{$c['city']}</td>
+          <td>{$c['state']}</td>
+          <td>{$c['zip']}</td>
+          <td>{$this->formatPhoneNumber($c['areaCode'], $c['phonePrefix'], $c['phoneLineNumber'])}</td>
+          <td>{$this->bool2txt($c['promotionEmail'])}</td>
+          <td>{$this->bool2txt($c['promotionText'])}</td>
+          <td>{$this->bool2txt($c['appointmentAlert'])}</td>
+        </tr>
+      ";
+    }
+    echo" 
+      </tbody>
+    </table>
+    ";
   }
 }
--- application/controllers/Stela.php
+++ application/controllers/Stela.php
@@ -3,21 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Stela extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
 		$this->load->view('stelaMain');
@@ -28,4 +13,8 @@ class Stela extends CI_Controller {
     $data['array'] = $a;
     $this->load->view('dumpArray', $data);
   }
+
+  public function formatPhoneNumber($a, $p, $l) { return "($a) $p-$l"; }
+
+  public function bool2txt($b) { return ($b) ? "Yes" : "No"; }
 }
