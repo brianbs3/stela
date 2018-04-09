dc328b037a305f58c4099a07f123f0e91f0ed7de
bsizemore <bsizemore@apple.com>
2018-04-09 11:58:54 -0400

    barcode

--- application/controllers/Barcode.php
+++ application/controllers/Barcode.php
@@ -5,5 +5,19 @@ require('TCPDF/tcpdf.php');
 class Barcode extends Stela {
   public function index()
   {
+    require_once('TCPDF/tcpdf_barcodes_1d.php');
+    $barcodeobj = new TCPDFBarcode('hello world', 'C128');
+    $barcodeobj->getBarcodePNG(2, 30, array(0,0,0));
+  }
+
+  public function getBarcode($code = 'bs', $encode = 'C128')
+  {
+//    $code = $this->input->get('code', true);
+ //   $encode = $this->input->get('encode', true);
+
+    require_once('TCPDF/tcpdf_barcodes_1d.php');
+    $barcodeobj = new TCPDFBarcode($code, $encode);
+    $barcodeobj->getBarcodePNG(2, 30, array(0,0,0));
+    
   }
 }
