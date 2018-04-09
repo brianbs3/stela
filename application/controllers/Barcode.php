21fb36c46d1b9eab19a9c86b4b7d04c36399af36
bsizemore <bsizemore@apple.com>
2018-04-09 14:04:21 -0400

    add barcode stuff

--- application/controllers/Barcode.php
+++ application/controllers/Barcode.php
@@ -1,23 +1,32 @@
 <?php
+
+/* 
+* Do some path massaging to get the barcode to work
+*/
+$dir = dirname(__FILE__, 3);
+set_include_path(get_include_path() . PATH_SEPARATOR . "$dir/Image");
+$path = get_include_path();
+
 require_once('Stela.php');
-require('application/libraries/fpdf.php');
-require('TCPDF/tcpdf.php');
+require_once("Image/Image/Barcode2.php");
+
 class Barcode extends Stela {
   public function index()
   {
-    require_once('TCPDF/tcpdf_barcodes_1d.php');
-    $barcodeobj = new TCPDFBarcode('hello world', 'C128');
-    $barcodeobj->getBarcodePNG(2, 30, array(0,0,0));
+    $code = $this->input->get('code', true);
+    $encode = $this->input->get('encode', true);
+    $img_type = $this->input->get('img_type', true);
+
+    if(!$encode)
+      $this->drawBarcode($code);
+    else if(!$img_type)
+      $this->drawBarcode($code, $encode);
+    else
+      $this->drawBarcode($code, $encode, $img_type);
   }
 
-  public function getBarcode($code = 'bs', $encode = 'C128')
+  public function drawBarcode($code = "", $encode = "code128", $img_type = "png")
   {
-//    $code = $this->input->get('code', true);
- //   $encode = $this->input->get('encode', true);
-
-    require_once('TCPDF/tcpdf_barcodes_1d.php');
-    $barcodeobj = new TCPDFBarcode($code, $encode);
-    $barcodeobj->getBarcodePNG(2, 30, array(0,0,0));
-    
+    Image_Barcode2::draw($code, $encode, $img_type, true, 15, 1);
   }
 }
