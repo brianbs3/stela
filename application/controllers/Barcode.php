8bcb1929d32f16c9653a4037601619cd04aa7833
bsizemore <bsizemore@apple.com>
2018-04-09 11:14:25 -0400

    init barcode class

--- application/controllers/Barcode.php
+++ application/controllers/Barcode.php
@@ -2,7 +2,7 @@
 require_once('Stela.php');
 require('application/libraries/fpdf.php');
 require('TCPDF/tcpdf.php');
-class Customers extends Stela {
+class Barcode extends Stela {
   public function index()
   {
   }
