f3cb2b2a7f4eec36b98114c338dc627b4e5bc04e
bsizemore <bsizemore@apple.com>
2018-04-12 17:00:47 -0400

    email

--- application/controllers/Email.php
+++ application/controllers/Email.php
@@ -7,4 +7,20 @@ class Email extends Stela {
   {
     echo"email";
   }
+
+  public function sendEmail()
+  {
+    $this->load->library('email');
+
+    $this->email->from('brianbs3@gmail.com', 'Brian Sizemore');
+    $this->email->to('bsizemore@apple.com');
+    $this->email->cc('bsizemore@apple.com');
+    //$this->email->bcc('them@their-example.com');
+    
+    $this->email->subject('Email Test');
+    $this->email->message('Testing the email class.');
+    //$this->email->message('<h1>Testing the email class.</h1>  Email....');
+    
+    $this->email->send();
+  }
 }
