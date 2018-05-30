6629077f7332c748b44ff9015fcffb9157d3b8b6
bsizemore <bsizemore@apple.com>
2018-05-30 13:37:29 -0400

    remove header and footer

--- application/controllers/CheckIn.php
+++ application/controllers/CheckIn.php
@@ -7,20 +7,11 @@ require_once('Stela.php');
 class CheckIn extends Stela {
     public function index()
     {
-        $startHour = 8;
-        $endHour = 19;
-        $minChunk = 15;
-        for($i = $startHour; $i <= $endHour; $i++) {
-            $h = ($i > 12) ? $i - 12 : $i;
-            for($j = 0; $j <= 45; $j++) {
-                if ($j % $minChunk == 0) {
-                    $m = ($j == 0) ? "00" : $j;
-                    $tbl .= "<tr><td>$h:$m</td></tr>";
-                }
-            }
-        }
+
         $pdf = new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, false, 'UTF-8', false);
         //$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+        $pdf->SetPrintHeader(false);
+        $pdf->SetPrintFooter(false);
         $pdf->SetMargins(20, 1, 20);
         $pdf->AddPage();
         $pdf->writeHTML("<span align=\"right\">Date: ___/___/______</span>", true, false, false, false, '');
