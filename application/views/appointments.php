76e40504a22115f3d35eb777a06689cf3633719b
bsizemore <bsizemore@apple.com>
2018-06-12 18:14:55 -0400

    start to configure calendar

--- application/views/appointments.php
+++ application/views/appointments.php
@@ -1,14 +1,40 @@
 <div class="container-fluid">
     <div class="row">
-        <div class="col-sm">
-          <div id='calendar'></div>
+        <div class="col-sm appointments-left">
         </div>
-        <div class="col-xl appointments-right">
+        <div class="col-lg appointments-right">
             col 2
         </div>
 </div>
+<br><br><hr><br>
+<div id='calendar'></div>
 <script type=text/javascript>
 $('#calendar').fullCalendar({
-  //weekends: false // will hide Saturdays and Sundays
-});
+    defaultView: 'month',
+
+    header: {
+      center: 'addEventButton'
+    },
+
+    customButtons: {
+      addEventButton: {
+        text: 'add event...',
+        click: function() {
+          var dateStr = prompt('Enter a date in YYYY-MM-DD format');
+          var date = moment(dateStr);
+
+          if (date.isValid()) {
+            $('#calendar').fullCalendar('renderEvent', {
+              title: 'dynamic event',
+              start: date,
+              allDay: true
+            });
+            alert('Great. Now, update your database...');
+          } else {
+            alert('Invalid date.');
+          }
+        }
+      }
+    }
+  });
 </script>
